96abf61c9cd0bf05318d142c73454f22903d3b66
Vinicius Teixeira <vinicius.escudero@outlook.com>
2025-07-23 15:50:49 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,10 @@
-cscript script.vbs "C:\scripts\OnePage\Dashboard of Operations.xlsm"
+@echo off
+setlocal
+
+:: Define the full path to the Excel macro file
+set FILE_PATH=%~dp0macros\Dashboard of Operations.xlsm
+
+:: Call the VBScript to open Excel and run the macro
+cscript //nologo script.vbs "%FILE_PATH%"
+
+endlocal
